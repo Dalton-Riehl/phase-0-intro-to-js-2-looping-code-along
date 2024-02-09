36214686f0ce3076ae6fdd6ccfb0757b1a0dd94d
Dalton Riehl <dtonriehl@gmail.com>
2024-02-08 22:11:41 -0600

    Looping Code-Along Hellscape From Satan's Butthole Complete: Achievement Get!

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function countDown(num1) {
+    while (num1 >= 0) {
+        console.log(num1);
+        num1--;
+    }
+}
+
+function writeCards(recipient, eventName) {
+    let newArray = [];
+    for (let i = 0; i < recipient.length; i++) {
+        let thankYou = `Thank you, ${recipient[i]}, for the wonderful ${eventName} gift!`;
+        newArray.push(thankYou);
+    }
+    return newArray;
+}
